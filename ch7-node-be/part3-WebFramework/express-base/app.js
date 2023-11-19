1b197d2e0db76d074dbf1fcc36145506e18b95cc
jsfullstac <thinkerr24@163.com>
2023-11-19 12:37:13 +0800

    finish express base example

--- ch7-node-be/part3-WebFramework/express-base/app.js
+++ ch7-node-be/part3-WebFramework/express-base/app.js
@@ -42,30 +42,70 @@ app.get("/todos/:id", async (req, res) => {
 });
 
 app.post("/todos", async (req, res) => {
-  // 1.获取客户端请求体参数
-  /**
-   {
-    "title": xxx 
-   }
-   */
-  const { title } = req.body;
-  // 2.数据校验
-  if (!title) {
-    res.status(422).json({
-      error: "The field title is required.",
-    });
-  } else {
-    // 3.操作数据库
-    const db = await getDb();
-    const lastTodo = db.todos[db.todos.length - 1];
-    db.todos.push({
-      id: (lastTodo ? lastTodo.id : 0) + 1,
-      title,
-    });
+  try {
+    // 1.获取客户端请求体参数
+    const { title } = req.body;
+    // 2.数据校验
+    if (!title) {
+      res.status(422).json({
+        error: "The field title is required.",
+      });
+    } else {
+      // 3.操作数据库
+      const db = await getDb();
+      const lastTodo = db.todos[db.todos.length - 1];
+      db.todos.push({
+        id: (lastTodo ? lastTodo.id : 0) + 1,
+        title,
+      });
 
-    await saveDb(db);
-    // 4.发送成功响应
-    res.status(200).json({ title });
+      await saveDb(db);
+      // 4.发送成功响应
+      res.status(201).json({ title });
+    }
+  } catch (err) {
+    res.status(500).json({
+      error: err.message,
+    });
+  }
+});
+
+app.patch("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { todo } = req.body;
+    const db = await getDb();
+    const findTodo = db.todos.find((todo) => String(todo.id) === id);
+    if (findTodo) {
+      Object.assign(findTodo, todo);
+      await saveDb(db);
+      res.status(200).json(findTodo);
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.status(500).json({
+      error: err.message,
+    });
+  }
+});
+
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const {id} = req.params;
+    const db = await getDb();
+    const index = db.todos.findIndex((todo) => String(todo.id) === id);
+    if (index > -1) {
+      db.todos.splice(index, 1);
+      await saveDb(db);
+      res.status(204).end();
+    } else {
+      res.status(404).end();
+    }
+  } catch(err) {
+    res.status(500).json({
+      error: err.message,
+    });
   }
 });
 
