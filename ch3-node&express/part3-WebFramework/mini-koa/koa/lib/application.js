5bdc714f0eb0b077b9642b6ec1ff523ef599f661
thinkerr24 <thinkerr24@163.com>
2023-11-26 10:47:31 +0800

    implement koa  part3

--- ch3-node&express/part3-WebFramework/mini-koa/koa/lib/application.js
+++ ch3-node&express/part3-WebFramework/mini-koa/koa/lib/application.js
@@ -1,7 +1,15 @@
 const http = require("http");
+const context = require("./context");
+const request = require("./request");
+const response = require("./response");
+
 class Applcation {
   constructor() {
     this.middleware = []; // 保存用户添加的中间件函数
+
+    this.context = Object.create(context);
+    this.request = Object.create(request);
+    this.response = Object.create(response);
   }
   listen(...args) {
     const server = http.createServer(this.callback());
@@ -15,7 +23,7 @@ class Applcation {
 
   // 异步递归遍历调用中间件处理函数
   compose(middleware) {
-    return function () {
+    return function (context) {
       const dispatch = (index) => {
         if (index >= middleware.length) {
           return Promise.resolve();
@@ -23,17 +31,23 @@ class Applcation {
         const fn = middleware[index];
         return Promise.resolve(
           // TODO: 上下文对象/ next
-          fn({}, () => dispatch(index + 1))
+          fn(context, () => dispatch(index + 1))
         );
       };
       // 返回第一个中间件处理函数
       return dispatch(0);
     };
   }
+
+  // 构造上下文对象
+  createContext() {}
+
   callback() {
     const fnMiddleware = this.compose(this.middleware);
     const handleRequest = (req, res) => {
-      fnMiddleware()
+      // 每个请求都会创建一个独立的Context上下文对象，它们之间不会互相污染
+      const newContext = this.createContext();
+      fnMiddleware(newContext)
         .then(() => {
           console.log("end");
           res.end("My Koa");
